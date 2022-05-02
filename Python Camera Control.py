32a63d7ac6a533ed3d6106f14497c41e307a9384
grand-kanyin <104409813+grand-kanyin@users.noreply.github.com>
2022-05-01 23:52:35 -0400

    Update Python Camera Control.py

--- Python Camera Control.py
+++ Python Camera Control.py
@@ -1,5 +1,12 @@
 import urllib.request
 
 webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_start","byValue":50}}}')
-   
+time.sleep(2)
+webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_stop","byValue":50}}}')
+time.sleep(2)
+webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_start","byValue":50}}}')
+time.sleep(5)
+webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_stop","byValue":50}}}')
+time.sleep(2)
+
 print("result code: " + str(webUrl.getcode()))
