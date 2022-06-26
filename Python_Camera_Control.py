ae369d2cb06fa1079ccea569dfad354ddac1027d
grand-kanyin <104409813+grand-kanyin@users.noreply.github.com>
2022-06-26 12:44:47 -0400

    Update Python_Camera_Control.py

--- Python_Camera_Control.py
+++ Python_Camera_Control.py
@@ -3,37 +3,126 @@ import urllib.request
 import time
 import sys
 
-def leftmovestart():
+# South Pole Camera 4 Moveset
+def leftmovestart4(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def rightmovestart4(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def upmovestart4(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"up_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"up_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def downmovestart4(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"down_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"down_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def presets4(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"preset_set","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def zoom_in4(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"zoomadd_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"zoomadd_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def zoom_out4(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":""zoomdec_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.67/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"z"zoomdec_stop,"byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+
+# Middle Camera 5 Moveset
+def leftmovestart5(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def rightmovestart5(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def upmovestart5(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"up_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"up_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def downmovestart5(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"down_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"down_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def presets5(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"preset_set","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def zoom_in5(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"zoomadd_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"zoomadd_stop","byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+def zoom_out5(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":""zoomdec_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.69/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"z"zoomdec_stop,"byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
+
+#South Pole Camera 6 Moveset
+def leftmovestart6(x):
     webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_start","byValue":50}}}')
-    time.sleep(2)
+    time.sleep(x)
     webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_stop","byValue":50}}}')
     return print("result code: " + str(webUrl.getcode()))
 
-def rightmovestart():
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_start","byValue":50}}}')
-    time.sleep(2)
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_stop","byValue":50}}}')
+def rightmovestart6(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"right_stop","byValue":50}}}')
     return print("result code: " + str(webUrl.getcode()))
 
-def upmovestart():
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"up_start","byValue":50}}}')
-    time.sleep(2)
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"up_stop","byValue":50}}}')
+def upmovestart6(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"up_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"up_stop","byValue":50}}}')
     return print("result code: " + str(webUrl.getcode()))
 
-def downmovestart():
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"down_start","byValue":50}}}')
-    time.sleep(2)
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"down_stop","byValue":50}}}')
+def downmovestart6(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"down_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"down_stop","byValue":50}}}')
     return print("result code: " + str(webUrl.getcode()))
 
-def presets():
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"preset_set","byValue":50}}}')
+def presets6(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"preset_set","byValue":50}}}')
     return print("result code: " + str(webUrl.getcode()))
 
-def zoom_in():
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"zoomadd_stop","byValue":50}}}')
-    time.sleep(2)
+def zoom_in6(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"zoomadd_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"zoomadd_stop","byValue":50}}}')
     return print("result code: " + str(webUrl.getcode()))
 
-
+def zoom_out6(x):
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":""zoomdec_start","byValue":50}}}')
+    time.sleep(x)
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"z"zoomdec_stop,"byValue":50}}}')
+    return print("result code: " + str(webUrl.getcode()))
