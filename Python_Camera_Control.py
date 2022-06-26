c654d08d72a3cb6a3a0e251b76c24bfea778533a
grand-kanyin <104409813+grand-kanyin@users.noreply.github.com>
2022-06-26 10:19:01 -0400

    Update Python_Camera_Control.py

--- Python_Camera_Control.py
+++ Python_Camera_Control.py
@@ -4,9 +4,9 @@ import time
 import sys
 
 def leftmovestart():
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_start","byValue":50}}}')
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_start","byValue":50}}}')
     time.sleep(2)
-    webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_stop","byValue":50}}}')
+    webUrl= urllib.request.urlopen('http://172.31.0.68/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"left_stop","byValue":50}}}')
     return print("result code: " + str(webUrl.getcode()))
 
 def rightmovestart():
@@ -35,5 +35,5 @@ def zoom_in():
     webUrl= urllib.request.urlopen('http://172.31.0.66/ajaxcom?szCmd={"SysCtrl":{"PtzCtrl":{"nChanel":0,"szPtzCmd":"zoomadd_stop","byValue":50}}}')
     time.sleep(2)
     return print("result code: " + str(webUrl.getcode()))
-    
+
 
