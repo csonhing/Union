fc38116f94c083ded7d9396fbafe361887be7489
grand-kanyin <104409813+grand-kanyin@users.noreply.github.com>
2022-06-26 12:46:01 -0400

    Update UI.py
    Ignore the elifs, I was just testing passing the move distance to and from the functions and how fast that happened and it seems fast enough

--- UI.py
+++ UI.py
@@ -1,3 +1,13 @@
-from Python_Camera_Control import leftmovestart
+from Python_Camera_Control import* 
 
-leftmovestart()
+cam = input('What camera do you want to move?\n')
+if cam == '4':
+    z = input('How far do you want to move?\n')
+    y = input('How fast do you want to move?\n')
+    rightmovestart4(float(z))
+elif cam == '5':
+    z = input('How far do you want to move?\n')
+    rightmovestart5(float(z))
+elif cam == '6':
+    z = input('How far do you want to move?\n')
+    rightmovestart6(float(z))  
