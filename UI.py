1c6fda0ab6942fae45ecc893547d473312ce22b7
grand-kanyin <104409813+grand-kanyin@users.noreply.github.com>
2022-09-04 01:21:31 -0400

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,13 +1,22 @@
+from turtle import left
 from Python_Camera_Control import* 
+from Learning import*  
+channel = "0,"
+direction = ["\"left_start\"," "\"leftup_start\"," "\"leftdown_start\"," "\"right_start\"," "\"rightup_start\"," "\"rightdown_start\"," "\"up_start\"," "\"down_start\"," "\"zoomadd_start\"," "\"zoomdec_start\"," "\"preset_call\","  ]
+IPs = ["172.31.0.66" "172.31.0.67" "172.31.0.68" "172.31.0.69"]
+speed = str(tuple(range(1, 100, 1)))
+x = 2
+command = "blank"
 
-cam = input('What camera do you want to move?\n')
-if cam == '4':
-    z = input('How far do you want to move?\n')
-    y = input('How fast do you want to move?\n')
-    rightmovestart4(float(z))
-elif cam == '5':
-    z = input('How far do you want to move?\n')
-    rightmovestart5(float(z))
-elif cam == '6':
-    z = input('How far do you want to move?\n')
-    rightmovestart6(float(z))  
+if left:
+    direction = "\"left_start\","
+    speed = #user input somehow with a slide bar or something
+    if IPs == "172.31.0.67":
+        leftmovestart4(x,command)
+        camip  = "172.31.0.67"
+        command = "http://"+ camip + "/ajaxcom?szCmd={\"SysCtrl\":{\"PtzCtrl\":{\"nChanel\":" + channel + "\"szPtzCmd\":" + direction + "\"byValue\":" + speed +"}}}"
+    else:
+        if IPs == "172.31.0.69":
+        leftmovestart5(x,command)
+        camip  = "172.31.0.67"
+        command = "http://"+ camip + "/ajaxcom?szCmd={\"SysCtrl\":{\"PtzCtrl\":{\"nChanel\":" + channel + "\"szPtzCmd\":" + direction + "\"byValue\":" + speed +"}}}"
